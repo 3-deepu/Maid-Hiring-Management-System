e0a7eda9b377919b98404e63b27c4b4320123238
3-deepu <150538961+3-deepu@users.noreply.github.com>
2025-07-16 19:05:44 +0530

    Delete mhms directory

--- mhms/admin/includes/footer.php
+++
@@ -1,5 +0,0 @@
- <div class="container-fluid">
-                     <div class="footer">
-                        <p>Maid Hiring Management System.</p>
-                     </div>
-                  </div>
